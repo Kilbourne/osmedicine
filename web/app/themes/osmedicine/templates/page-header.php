e8ee3e6a3821a8202785feaeb499b7de819fcf95
Kilbourne <luca.castellone@gmail.com>
2016-04-13 17:59:31 +0200

    allegati pdf

--- web/app/themes/osmedicine/templates/page-header.php
+++ web/app/themes/osmedicine/templates/page-header.php
@@ -1,5 +1,5 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header" hidden>
+<div class="page-header" <?php 	if(! is_page('allegati')) echo 'hidden'; ?>>
   <h1><?= Titles\title(); ?></h1>
 </div>
