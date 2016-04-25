b2825d59fa315543c49940ca31bfe1766d540e5b
Kilbourne <luca.castellone@gmail.com>
2016-04-26 00:02:33 +0200

    assett optimization

--- web/app/themes/osmedicine/lib/setup.php
+++ web/app/themes/osmedicine/lib/setup.php
@@ -125,16 +125,18 @@ $detect = new Mobile_Detect;
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+  wp_enqueue_style('sage-css', Assets\asset_path('styles/main.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
-  wp_localize_script( 'sage/js', 'OSM', array(
+  wp_enqueue_script('sage-js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_localize_script( 'sage-js', 'OSM', array(
         'ajaxurl'   => admin_url( 'admin-ajax.php' ),
     'nonce'     => wp_create_nonce( 'OSM_validate_code-nonce' ))
   );
+  $css_target = 'img.'. $bodhi_svgs_options['css_target'];
+wp_localize_script( 'sage-js', 'cssTarget', $css_target );
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
