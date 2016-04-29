72c40564e6f8b34c24f43d77811f6f2ed90fa253
Kilbourne <luca.castellone@gmail.com>
2016-04-29 11:10:07 +0200

    AAAA

--- web/app/themes/osmedicine/page-allegati.php
+++ web/app/themes/osmedicine/page-allegati.php
@@ -1,4 +1,3 @@
-
 <?php 
 use Roots\Sage\Extras;
 use Roots\Sage\Setup;
@@ -20,8 +19,8 @@ function posts_by_year_plus_category() {
 	  	
 	  	if(! is_array($years[date('Y', strtotime($allegato->post_date))])) $years[date('Y', strtotime($allegato->post_date))] = array();
 	  	
-	  	if(! is_array($years[date('Y', strtotime($allegato->post_date))][$category])) $years[date('Y', strtotime($allegato->post_date))][$category] = array();
-	    $years[date('Y', strtotime($allegato->post_date))][$category][] = $allegato->ID;
+	  	//if(! is_array($years[date('Y', strtotime($allegato->post_date))][$category])) $years[date('Y', strtotime($allegato->post_date))][$category] = array();
+	    $years[date('Y', strtotime($allegato->post_date))][] = $allegato->ID;
 	  }
   }
   // reverse sort by year
@@ -38,59 +37,30 @@ get_template_part('templates/page', 'header');
   <?php 
     }
 if ($years) {
-	echo '<p class="allegati-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	</p><section class="allegati-wrapper">
-		<table>				
-			<tbody>
+	echo '<p class="allegati-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	</p>
+  <section class="allegati-wrapper">
+
 				
 				
 				';
 	
 
     foreach ($years as $key=>$anno) { ?>
-    <tr class="year-row">
-    	<td	>
-    		<time class="anno"><?php echo $key; ?></time>	
-    		<table>	    			
-    			<tbody>
-    			<?php foreach ($anno as $category=>$allegati_arr) { ?>
-    				<tr>    				
-     					<td class="allegati-legend">
-    						<div class="allegato_category <?php 	echo strtolower($category).'-cat-bg-color';?> "><?php echo $category; ?></div>
-    					</td>
-    					<td class="allegati-list">
-		    	 			<?php foreach ($allegati_arr as $key2=>$allegato_id) {  
-		    	 				$allegato = get_post($allegato_id);
-				 				$file=get_field('file_allegato',$allegato_id)['url'];
-		    	 		?>
-								<div><a href=" <?php 	echo $file ; ?>	"><h5><?php 	echo get_the_title( $allegato_id ); ?>	</h5><?php echo get_the_post_thumbnail( $allegato_id ); ?></a></div>
-		    	 			<?php } ?>		    	 			
-    					</td>     				
-    				</tr>
-    				<tr>
-    						<td	></td>
-    				 	<td>
-    				 		<hr>
-    				 	</td>
-    				 </tr>	 
-    				<?php }	 ?>
-    			</tbody>
-    		</table>
-
-    	</td>
-    	
-			
-				
-				 	
-			
-
-    	
-    	
-	</tr>
-	 	<?php
-				 } 
-    		 echo'
-			</tbody>
-		</table></section>';
+    <ul class="year-row allegati-list">
+      <time class="anno"><?php echo $key; ?></time> 
+      <?php foreach ($anno as $key2=>$allegato_id ) { 
+                  $allegato = get_post($allegato_id);
+                $file=get_field('file_allegato',$allegato_id)['url'];
+              ?>
+                <li>
+                <div><a href=" <?php  echo $file ; ?> "><h5><?php   echo get_the_title( $allegato_id ); ?>  </h5><?php echo get_the_post_thumbnail( $allegato_id ); ?></a></div>
+                 </li>
+                <?php } ?>                
+     
+    </ul>
+    <?php } ?>
+    </section>
+    <?php
 }else{ ?>
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'sage'); ?>
