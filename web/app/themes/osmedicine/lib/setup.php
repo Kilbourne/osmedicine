b3aa9f07489973e9b61b9cbba99be7b95c78f39e
Kilbourne <luca.castellone@gmail.com>
2016-05-02 01:00:42 +0200

    update

--- web/app/themes/osmedicine/lib/setup.php
+++ web/app/themes/osmedicine/lib/setup.php
@@ -136,7 +136,7 @@ function assets() {
         'ajaxurl'   => admin_url( 'admin-ajax.php' ),
     'nonce'     => wp_create_nonce( 'OSM_validate_code-nonce' ))
   );
-  $css_target = 'img.'. $bodhi_svgs_options['css_target'];
+  $css_target = isset($bodhi_svgs_options['css_target']) ?'img.'. $bodhi_svgs_options['css_target']:'img.';
 wp_localize_script( 'sage-js', 'cssTarget', $css_target );
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
--- web/app/themes/osmedicine/page-allegati.php
+++ web/app/themes/osmedicine/page-allegati.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 use Roots\Sage\Extras;
 use Roots\Sage\Setup;
 function posts_by_year_plus_category() {
@@ -14,49 +14,49 @@ function posts_by_year_plus_category() {
   if($allegati){
 	  foreach($allegati as $allegato) {
 	  	$category = wp_get_post_categories( $allegato->ID );
-	  	
+
 	  	$category = count($category)>0?get_category($category[0])->name:'-';
-	  	
-	  	if(! is_array($years[date('Y', strtotime($allegato->post_date))])) $years[date('Y', strtotime($allegato->post_date))] = array();
-	  	
+
+	  	if(! isset($years[date('Y', strtotime($allegato->post_date))])) $years[date('Y', strtotime($allegato->post_date))] = array();
+
 	  	//if(! is_array($years[date('Y', strtotime($allegato->post_date))][$category])) $years[date('Y', strtotime($allegato->post_date))][$category] = array();
 	    $years[date('Y', strtotime($allegato->post_date))][] = $allegato->ID;
 	  }
   }
   // reverse sort by year
-  
+
 
   return $years;
 }
 $years = posts_by_year_plus_category();
-get_template_part('templates/page', 'header'); 
+get_template_part('templates/page', 'header');
 	    if (Setup\display_sidebar_mobile() ){ ?>
           <aside class="sidebar">
             <?php get_template_part('templates/sidebar-mobile'); ?>
           </aside><!-- /.sidebar -->
-  <?php 
+  <?php
     }
 if ($years) {
 	echo '<p class="allegati-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	</p>
   <section class="allegati-wrapper">
 
-				
-				
+
+
 				';
-	
+
 
     foreach ($years as $key=>$anno) { ?>
     <ul class="year-row allegati-list">
-      <time class="anno"><?php echo $key; ?></time> 
-      <?php foreach ($anno as $key2=>$allegato_id ) { 
+      <time class="anno"><?php echo $key; ?></time>
+      <?php foreach ($anno as $key2=>$allegato_id ) {
                   $allegato = get_post($allegato_id);
-                $file=get_field('file_allegato',$allegato_id)['url'];
+                $file=get_field('file_allegato',$allegato_id)?get_field('file_allegato',$allegato_id):false;
               ?>
                 <li>
                 <div><a href=" <?php  echo $file ; ?> "><h5><?php   echo get_the_title( $allegato_id ); ?>  </h5><?php echo get_the_post_thumbnail( $allegato_id ); ?></a></div>
                  </li>
-                <?php } ?>                
-     
+                <?php } ?>
+
     </ul>
     <?php } ?>
     </section>
@@ -68,7 +68,7 @@ if ($years) {
   <?php get_search_form(); ?>
 <?php
 
-} 
+}
 
 
  ?>
