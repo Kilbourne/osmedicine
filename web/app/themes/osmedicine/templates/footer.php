3979b37b8297dbe4b71faacdb25d91b1ad19860f
Kilbourne <luca.castellone@gmail.com>
2016-05-10 18:04:10 +0200

    last things

--- web/app/themes/osmedicine/templates/footer.php
+++ web/app/themes/osmedicine/templates/footer.php
@@ -2,15 +2,28 @@
 <footer class="content-info">
   <div class="container">
     <?php //dynamic_sidebar('sidebar-footer'); ?>
-    	<div class="info-rivista">
-     		<p>Open Source in Medicine</p>
+    	<div class="info-rivista">     		
      		<p>Testata giornalistica in attesa di registrazione presso il Tribunale di Napoli (R.G. 2732/16)</p>
      	</div>
      	<div class="footer-menu">
             <span><a href="<?= Extras\get_page_url_by_name('Chi siamo') ?>">Chi siamo</a></span><span><a href="<?= Extras\get_page_url_by_name('Contatti') ?>">Contatti</a></span><?php if(!is_user_logged_in ()){ ?><span><a href="<?= Extras\get_page_url_by_name('Login') ?>">Login</a></span><span><a href="<?= Extras\get_page_url_by_name('Registrazione') ?>">Registrati</a></span><?php }else{ ?><span><a href="<?= Extras\get_page_url_by_name('Guarda profilo') ?>">Profilo</a></span><?php } ?> <span><a href="<?= Extras\get_page_url_by_name('Privacy Policy') ?>">Privacy Policy</a></span><span><a href="<?= Extras\get_page_url_by_name('Termini e Condizioni') ?>">Termini e Condizioni</a></span>
      	</div>
      	<div class="info-menthalia">
-     		<p>© 2016 Menthalia srl PI 000000000  |  Piazzale Tecchio 49, 80125  Napoli</p>
+     		<p>© 2016 Menthalia srl PI 06980851213  |  Piazzale Tecchio 49, 80125  Napoli</p>
      	</div> 	
   </div>
+  <?php 
+  if(get_current_blog_id() === 1){
+    $analytics_code='UA-77520161-1';
+  }elseif(get_bloginfo( 'name' )==="Open Source in Virology" ){
+    $analytics_code='UA-77531534-1';
+  }
+   ?>
+  <script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', <?php echo $analytics_code; ?>, 'auto');
+ga('send', 'pageview');
+</script>
+<script async src='https://www.google-analytics.com/analytics.js'></script>
+
 </footer>
--- web/app/themes/osmedicine/templates/page-header.php
+++ web/app/themes/osmedicine/templates/page-header.php
@@ -1,5 +1,5 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header" <?php 	if(! is_page('allegati')) echo 'hidden'; ?>>
+<div class="page-header" <?php 	if(! in_array(true, array(is_page('allegati'),is_page('chi-siamo'),is_page('contatti'),is_page('privacy-policy'),is_page('termini_e_condizioni'))) ) echo 'hidden'; ?>>
   <h1><?= Titles\title(); ?></h1>
 </div>
